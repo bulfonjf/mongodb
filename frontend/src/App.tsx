2c9493ff4863df78749129fdd0bb25eeaf4db2b8
bulfonjf <bulfonjf@gmail.com>
2024-05-08 17:50:43 -0300

    Add user authentication test 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { useQuery } from "@apollo/client";
+import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
+import { Toaster } from "react-hot-toast";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-red-500 text-5xl bg-blue-500">Hola</h1>
-    </>
-  );
+	const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
+
+	if (loading) return null;
+
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={data.authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!data.authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!data.authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route path='*' element={<NotFoundPage />} />
+			</Routes>
+			<Toaster />
+		</>
+	);
 }
 
 export default App;
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,48 @@
+import { MdLogout } from "react-icons/md";
+import toast from "react-hot-toast";
+import { useMutation, useQuery } from "@apollo/client";
+import { LOGOUT } from "../graphql/mutations/user.mutation";
+import { GET_AUTHENTICATED_USER } from "../graphql/queries/user.query";
+
+
+const HomePage = () => {
+	const { data: authUserData } = useQuery(GET_AUTHENTICATED_USER);
+
+	const [logout, { loading, client }] = useMutation(LOGOUT, {
+		refetchQueries: ["GetAuthenticatedUser"],
+	});
+
+	const handleLogout = async () => {
+		try {
+			await logout();
+			// Clear the Apollo Client cache FROM THE DOCS
+			// https://www.apollographql.com/docs/react/caching/advanced-topics/#:~:text=Resetting%20the%20cache,any%20of%20your%20active%20queries
+			client.resetStore();
+		} catch (error) {
+			console.error("Error logging out:", error);
+			toast.error(error.message);
+		}
+	};
+
+	return (
+		<>
+			<div className='flex flex-col gap-6 items-center max-w-7xl mx-auto z-20 relative justify-center'>
+				<div className='flex items-center'>
+					<p className='md:text-4xl text-2xl lg:text-4xl font-bold text-center relative z-50 mb-4 mr-4 bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-400 inline-block text-transparent bg-clip-text'>
+						Spend wisely, track wisely
+					</p>
+					<img
+						src={authUserData?.authUser.profilePicture}
+						className='w-11 h-11 rounded-full border cursor-pointer'
+						alt='Avatar'
+					/>
+					{!loading && <MdLogout className='mx-2 w-5 h-5 cursor-pointer' onClick={handleLogout} />}
+					{/* loading spinner */}
+					{loading && <div className='w-6 h-6 border-t-2 border-b-2 mx-2 rounded-full animate-spin'></div>}
+				</div>
+				
+			</div>
+		</>
+	);
+};
+export default HomePage;
