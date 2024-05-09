fdea1a4cddf4971eb5759d0e455a06a23a7ac38b
bulfonjf <bulfonjf@gmail.com>
2024-05-09 00:56:38 -0300

    Test1 useQuery apollo client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,32 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
-import NotFoundPage from "./pages/NotFoundPage";
+// import { Navigate, Route, Routes } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+// import LoginPage from "./pages/LoginPage";
+// import SignUpPage from "./pages/SignUpPage";
+// import NotFoundPage from "./pages/NotFoundPage";
 import { useQuery } from "@apollo/client";
 import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
-import { Toaster } from "react-hot-toast";
+// import { Toaster } from "react-hot-toast";
 
 function App() {
-	const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
+	const { loading, error, data } = useQuery(GET_AUTHENTICATED_USER);
 
-	if (loading) return null;
+	if (loading) return "Loading...";
 
-	return (
-		<>
-			<Routes>
-				<Route path='/' element={data.authUser ? <HomePage /> : <Navigate to='/login' />} />
-				<Route path='/login' element={!data.authUser ? <LoginPage /> : <Navigate to='/' />} />
-				<Route path='/signup' element={!data.authUser ? <SignUpPage /> : <Navigate to='/' />} />
-				<Route path='*' element={<NotFoundPage />} />
-			</Routes>
-			<Toaster />
-		</>
-	);
+  if (error) return `Error! ${error.message}`;
+
+  return (<p>{JSON.stringify(data)}</p>);
+
+// 	return (
+// 		<>
+// 			<Routes>
+// 				<Route path='/' element={data.authUser ? <HomePage /> : <Navigate to='/login' />} />
+// 				<Route path='/login' element={!data.authUser ? <LoginPage /> : <Navigate to='/' />} />
+// 				<Route path='/signup' element={!data.authUser ? <SignUpPage /> : <Navigate to='/' />} />
+// 				<Route path='*' element={<NotFoundPage />} />
+// 			</Routes>
+// 			<Toaster />
+// 		</>
+// 	);
 }
 
 export default App;
